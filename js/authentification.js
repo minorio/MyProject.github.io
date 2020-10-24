f061cdb11eee1875b45e91a118af50743796dff4
Marina <58264033+minorio@users.noreply.github.com>
2020-10-24 12:16:16 +0300

    Исправила верстку форм

--- js/authentification.js
+++ js/authentification.js
@@ -1,34 +1,60 @@
 
 (function(){
     const firebaseConfig = {
-    apiKey: "AIzaSyBfONb2y-i_0JO8_1M1JL_mc_20dsqzjqc",
-    authDomain: "myproject-254a0.firebaseapp.com",
-    databaseURL: "https://myproject-254a0.firebaseio.com",
-    projectId: "myproject-254a0",
-    storageBucket: "myproject-254a0.appspot.com",
-    messagingSenderId: "1020357325084",
-    appId: "1:1020357325084:web:0b7d62baa87b25a0b4cad9"
-  };
-  firebase.initializeApp(firebaseConfig);
-  const txtLogin = document.getElementById('txtLogin');
-  const txtPass = document.getElementById('txtPassword');
-  const btnLogin = document.getElementById('btnLogin');
+        apiKey: "AIzaSyBgVckiwDxHryAhOI-RDJ6PmmAK4sKrjIQ",
+        authDomain: "mygameland-d3269.firebaseapp.com",
+        databaseURL: "https://mygameland-d3269.firebaseio.com",
+        projectId: "mygameland-d3269",
+        storageBucket: "mygameland-d3269.appspot.com",
+        messagingSenderId: "17717778700",
+        appId: "1:17717778700:web:54c68a99eaa92138361129"
+      }; firebase.initializeApp(firebaseConfig);
+//   const txtLogin = document.getElementById('txtLogin');
+//   const txtPass = document.getElementById('txtPassword');
+//   const btnLogin = document.getElementById('btnLogin');
   
- btnLogin.addEventListener('click',(event) => {
- const Login = txtLogin.value;
- const Pass = txtPass.value;
- const auth = firebase.auth();
- const promise = auth.singInWithEmailAndPassword(Login, Pass );
- promise.catch(event => alert(event.message))
+//  btnLogin.addEventListener('click',(event) => {
+//  const Login = txtLogin.value;
+//  const Pass = txtPass.value;
+//  const auth = firebase.auth();
+//  const promise = auth.singInWithEmailAndPassword(Login, Pass );
+//  promise.catch(event => alert(event.message))
+// });
+// firebase.auth().onAuthStateChanged(firebaseUser => {
+//     if(firebaseUser){
+//         console.log(firebaseUser);
+//     }
+//     else{
+//    alert('Пользователя не существует');
+//     }
+// }
+// )
+const txtLogin = document.getElementById('txtLogin');
+const txtPass = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnLogout = document.getElementById('btnLogout');
+
+btnLogin.addEventListener('click', e => {
+const email = txtLogin.value;
+const pass = txtPass.value;
+const auth = firebase.auth();
+
+const promise = auth.signInWithEmailAndPassword(email, pass);
+promise.catch(e => alert("Такого пользователя не существует"));
+ 
 });
+
+
 firebase.auth().onAuthStateChanged(firebaseUser => {
     if(firebaseUser){
-        console.log(firebaseUser);
+    console.log(firebaseUser);
+
+    } else {
+        console.log('Вы не вошли в аккаунт');
     }
-    else{
-    console.log('Пользователя не существует');
-    }
-}
-)
- 
-}()) ;
+});
+btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+  });
+}());
+
--- js/registrationforms.js
+++ js/registrationforms.js
@@ -1,40 +1,39 @@
-(function(){
-const firebaseConfig = {
-    apiKey: "AIzaSyBfONb2y-i_0JO8_1M1JL_mc_20dsqzjqc",
-    authDomain: "myproject-254a0.firebaseapp.com",
-    databaseURL: "https://myproject-254a0.firebaseio.com",
-    projectId: "myproject-254a0",
-    storageBucket: "myproject-254a0.appspot.com",
-    messagingSenderId: "1020357325084",
-    appId: "1:1020357325084:web:0b7d62baa87b25a0b4cad9"
-  };
-  firebase.initializeApp(firebaseConfig);
-  const txtLogin = document.getElementById('txtLogin');
-  const txtPass = document.getElementById('txtPassword');
-  const txtValidPass = document.getElementById('txtValidPassword');
-  const btnReg = document.getElementById('btnReg');
-  
+(function () {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBgVckiwDxHryAhOI-RDJ6PmmAK4sKrjIQ",
+    authDomain: "mygameland-d3269.firebaseapp.com",
+    databaseURL: "https://mygameland-d3269.firebaseio.com",
+    projectId: "mygameland-d3269",
+    storageBucket: "mygameland-d3269.appspot.com",
+    messagingSenderId: "17717778700",
+    appId: "1:17717778700:web:54c68a99eaa92138361129"
+  }; firebase.initializeApp(firebaseConfig);
+  const txtLogin = document.getElementById("txtLogin");
+  const txtPass = document.getElementById("txtPassword");
+  const txtValidPass = document.getElementById("txtValidPassword");
+  const btnReg = document.getElementById("btnReg");
 
-btnReg.addEventListener('click',(event) => {
+  btnReg.addEventListener("click", (event) => {
     const Login = txtLogin.value;
     const Pass = txtPass.value;
     const ValidPass = txtValidPass.value;
     const auth = firebase.auth();
-    if(Pass === ValidPass){
-       const promise = auth.createUserWithEmailAndPassword(Login, Pass);
-    promise.catch(event => alert(event.message))
-    }else{
-        alert('Пароли не совпадают')
-    }
-   
-});
-firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser){
-    console.log(firebaseUser);
- //    btnLogout.classList.remove('hide');
+    if (Pass === ValidPass) {
+      const promise = auth.createUserWithEmailAndPassword(Login, Pass);
+      promise.catch((event) => alert(event.message));
     } else {
-        console.log('Вы не вошли в аккаунт');
-     //    btnLogout.classList.add('hide');
+      alert("Пароли не совпадают");
     }
-});
+  });
+  firebase.auth().onAuthStateChanged((firebaseUser) => {
+   
+    if (firebaseUser) {
+      console.log(firebaseUser);
+      console.log("Вы  вошли в аккаунт");
+      //    btnLogout.classList.remove('hide');
+    } else {
+      console.log("Вы не вошли в аккаунт");
+      //    btnLogout.classList.add('hide');
+    }
+  });
 }());
