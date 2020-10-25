a68d49df3c790bf9b2a80574b6f0dda3c2baec19
QQ <71001778+Quuuod@users.noreply.github.com>
2020-10-25 14:15:32 +0300

    Загрузка статей

---
+++ js/upload.js
@@ -0,0 +1,80 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBu56aAvFADM1NaJirhZ3RB77SuTaO-fg",
+  authDomain: "myproject-df916.firebaseapp.com",
+  databaseURL: "https://myproject-df916.firebaseio.com",
+  projectId: "myproject-df916",
+  storageBucket: "myproject-df916.appspot.com",
+  messagingSenderId: "974154891845",
+  appId: "1:974154891845:web:0a87487e76bb751e22992c"
+};
+  firebase.initializeApp(firebaseConfig);
+
+ //получаем значения полей
+    const uploader = document.getElementById('uploader');
+  const fileButton = document.getElementById('fileButton');
+  let task, PictureURL;
+
+
+  //событие выбора файла
+  fileButton.addEventListener('change', function(e){
+      
+    //получаем файл
+  const file = e.target.files[0];
+
+  //создаём ссылку на хранилище 
+  const storageRef = firebase.storage().ref('Images/' + file.name);
+
+  //загружаем файл в хранилище
+  task = storageRef.put(file);
+
+  //обновляем полосочку загрузки
+    task.on('state_changed',
+      
+    function progress(snapshot){
+      
+      const percentage = (snapshot.bytesTransferred / 
+        snapshot.totalBytes) * 100;
+        uploader.value = percentage;
+   
+    },
+    function error(err){
+      alert(err.message)
+    },
+    function complete(){
+    }
+
+    );
+
+  });
+
+  document.getElementById('Post').onclick = function(e){  
+      
+    let Headingtxt = document.getElementById('txtHeading').value;
+      let New = document.getElementById('txtNew').value; 
+      let Now =  new Date().toLocaleString();;
+      let User;
+      firebase.auth().onAuthStateChanged(firebaseUser => {
+      if(firebaseUser){User =  firebase.auth().currentUser.email} else{User = 'Анонимная публикация'};
+      });
+  task.snapshot.ref.getDownloadURL().then(function(url){
+    PictureURL = url;
+
+    firebase.database().ref('Новости/'+ Headingtxt).set({             
+    Ссылка: PictureURL,
+    Заголовок: Headingtxt,
+    Текст_новости: New, 
+    Время_публикации: Now,  
+    Пользователь: User     
+     }); 
+  });
+}
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser){
+  console.log(firebaseUser);
+  console.log(firebase.auth().currentUser.email);
+
+  } else {
+      console.log('Вы не вошли в аккаунт');
+
+  }
+});
