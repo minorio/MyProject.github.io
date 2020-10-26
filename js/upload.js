75f08553bc1b7e6f62575cab132ddd0c17905d87
QQ <71001778+Quuuod@users.noreply.github.com>
2020-10-27 01:06:16 +0300

    Исправлено ли?

--- js/upload.js
+++ js/upload.js
@@ -1,14 +1,13 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBgVckiwDxHryAhOI-RDJ6PmmAK4sKrjIQ",
-  authDomain: "mygameland-d3269.firebaseapp.com",
-  databaseURL: "https://mygameland-d3269.firebaseio.com",
-  projectId: "mygameland-d3269",
-  storageBucket: "mygameland-d3269.appspot.com",
-  messagingSenderId: "17717778700",
-  appId: "1:17717778700:web:54c68a99eaa92138361129"
-}; 
 
-  firebase.initializeApp(firebaseConfig);
+  const firebaseConfig = {
+    apiKey: "AIzaSyBgVckiwDxHryAhOI-RDJ6PmmAK4sKrjIQ",
+    authDomain: "mygameland-d3269.firebaseapp.com",
+    databaseURL: "https://mygameland-d3269.firebaseio.com",
+    projectId: "mygameland-d3269",
+    storageBucket: "mygameland-d3269.appspot.com",
+    messagingSenderId: "17717778700",
+    appId: "1:17717778700:web:54c68a99eaa92138361129"
+  }; firebase.initializeApp(firebaseConfig);
 
  //получаем значения полей
     const uploader = document.getElementById('uploader');
@@ -52,10 +51,10 @@ const firebaseConfig = {
       
     let Headingtxt = document.getElementById('txtHeading').value;
       let New = document.getElementById('txtNew').value; 
-      let Now =  new Date().toLocaleString();
+      let Now =  new Date().toLocaleString();;
       let User;
       firebase.auth().onAuthStateChanged(firebaseUser => {
-      if(firebaseUser){User =  firebase.auth().currentUser.email} else{User = 'Анонимный пользователь'};
+      if(firebaseUser){User =  firebase.auth().currentUser.email} else{User = 'Анонимная публикация'};
       });
   task.snapshot.ref.getDownloadURL().then(function(url){
     PictureURL = url;
@@ -69,3 +68,13 @@ const firebaseConfig = {
      }); 
   });
 }
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser){
+  console.log(firebaseUser);
+  console.log(firebase.auth().currentUser.email);
+
+  } else {
+      console.log('Вы не вошли в аккаунт');
+
+  }
+});
